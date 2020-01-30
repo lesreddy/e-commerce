a96a4c9c8069292e1c067972be05f863268dcf01
lesreddy <les.reddy@gmail.com>
2020-01-30 11:46:10 +0000

    stripe working correctly!

--- checkout/models.py
+++ checkout/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from products.models import Product
 
 # Create your models here.
-
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=30, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
@@ -24,4 +23,5 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.product.name, self.product.price)
